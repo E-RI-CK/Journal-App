01102cfce01c23dddabf0f1eb6ab00270640d1c4
E-RI-CK <74332121+E-RI-CK@users.noreply.github.com>
2023-09-07 11:16:27 -0500

    favicon updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://E-RI-CK.github.io/Journal-App"
+  //base: "https://E-RI-CK.github.io/Journal-App"
 })
